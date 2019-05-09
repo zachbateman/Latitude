4197e1b8206f0f16d9a063c483515d858e389b84
zachbateman <39414345+zachbateman@users.noreply.github.com>
2019-05-09 16:07:10 -0500

    Formatting and clean up DataFrame creation loop
    Could create the DataFrame in one line...

--- LatOBA.py
+++ LatOBA.py
@@ -8,6 +8,7 @@ Enddate= "05/07/2019"
 driver = webdriver.Chrome()
 #"C:\Users\camoruso\Documents\Helpful Docs\Programming")
 driver.get('https://www.gasstorage.net/WORSHAMSTEED/Operator/index.cfm')
+
 element = driver.find_element_by_xpath('//*[@id="username"]')
 element.send_keys("camoruso")
 
@@ -31,23 +32,14 @@ element.send_keys(Enddate)
 element = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/fieldset[3]/form/table/tbody/tr/td[5]/input')
 element.send_keys(Keys.RETURN)
 
-#element = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/table/tbody')
-#for row in driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/table'):
-#	tds = tr.find_elements_by_tag_name('td')
-#	print([td.text for td in tds])
-
 table = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/table/tbody')
 table2 = driver.find_elements_by_xpath('//*[@id="content"]/div/div[3]/table/tbody/tr')
 rows = table.find_elements_by_tag_name("tr")
 print(rows)
-x= []
+
+x = []
 for row in rows:
-	print([td.text for td in row.find_elements_by_tag_name("td")])
-	a = [td.text for td in row.find_elements_by_tag_name("td")]
-	#dictionary = dict(zip(a,a))
-	#print(dictionary)	
-	x.append(a)
+	x.append([td.text for td in row.find_elements_by_tag_name("td")])
 
 df = pd.DataFrame(x)
 print(df)
-
