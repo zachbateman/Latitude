1a6ca652316a39f8d4dd58881586fcd26d2024b6
amorusochris <48426325+amorusochris@users.noreply.github.com>
2019-05-14 16:48:24 -0500

    Add files via upload

--- LatOBA.py
+++ LatOBA.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import xlsxwriter
-import xlrd
+import openpyxl
 import datetime
 
 #Server File location
@@ -12,7 +12,8 @@ loc2 = ('S:\OPERATIONS & ENGINEERING\Well DR Pressure\Dashboard - Wells.xlsm')
 #loc2 =('C:\\Users\camoruso\Desktop\Dashboard - Wells.xlsm')
 
 sheetname = "WS Alt Data"
-#login Info
+
+#login Info from local file
 F1 = open("C:\\Users\camoruso\Desktop\password.txt", "r")
 contents = F1.readlines()
 username = contents[0]
@@ -20,19 +21,13 @@ password = contents[1]
 F1.close()
 
 #Open Worksheet with Pandas
-dfexcel = pd.read_excel(loc2, sheet_name = sheetname)
-numrows = len(dfexcel.index)
-#print(numrows, "numrows")
-columns=dfexcel.columns
-#print(columns)
+wellsdf = pd.read_excel(loc2, sheet_name = sheetname)
+Excelrowcount = len(wellsdf.index)
+columns = wellsdf.columns
 column = columns[0]
-#print(column)
-Datelist = dfexcel[column].tolist()
-#print(c)
-#print(Datelist)
+Datelist = wellsdf[column].tolist()
 last = len(Datelist)
 lastdate = Datelist[last-1]
-#print(lastdate.date())
 
 today = datetime.date.today()
 Yesterday = datetime.date.today() + datetime.timedelta(days=-1)
@@ -50,6 +45,7 @@ else:
 driver = webdriver.Chrome()
 #"C:\Users\camoruso\Documents\Helpful Docs\Programming")
 driver.get('https://www.gasstorage.net/WORSHAMSTEED/Operator/index.cfm')
+
 element = driver.find_element_by_xpath('//*[@id="username"]')
 element.send_keys(username)
 
@@ -74,28 +70,52 @@ element = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/fieldset[3
 element.send_keys(Keys.RETURN)
 
 table = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/table/tbody')
-table2 = driver.find_elements_by_xpath('//*[@id="content"]/div/div[3]/table/tbody/tr')
+#table2 = driver.find_elements_by_xpath('//*[@id="content"]/div/div[3]/table/tbody/tr')
 rows = table.find_elements_by_tag_name("tr")
-print(rows)
+
 x= []
 for row in rows:
-	print([td.text for td in row.find_elements_by_tag_name("td")])
-	a = [td.text for td in row.find_elements_by_tag_name("td")]
-	#dictionary = dict(zip(a,a))
-	#print(dictionary)	
-	x.append(a)
+	x.append([td.text for td in row.find_elements_by_tag_name("td")])
 
-df = pd.DataFrame(x)
-#Count number of rows copied over
-numrows2 = len(df.index)
-print(df[1:numrows2-1])
-#Change dataframe to chop off header and total row
-df2 = df[1:numrows2-1]
+OBAdf = pd.DataFrame(x)
+
+#Close webdriver
 driver.close()
 driver.quit()
 
+#Count number of rows copied over
+obadatarows = len(OBAdf.index)
+#print(df[1:numrows2-1])
+
+#Change dataframe to chop off header and total row
+OBAdf2 = OBAdf[1:obadatarows-1]
+print(OBAdf2.dtypes)
+
+#for col in OBAdf2.columns[2:]:
+#	OBAdf2[col] = OBAdf2[col].apply(pd.to_numeric)
+	#convert_object(convert_numeric = True)
+#print(OBAdf2.dtypes)
+	
 #output
 #print(df[1:numrows2])
-#writer = pd.Excelwriter(localloc)
+#writer = pd.Excelwriter(loc2)
 #Write to excel after the number of entries from origina numrows (Excel sheet count)
-df2.to_excel(loc2,sheetname,startrow=numrows+2)
+#with pd.ExcelWriter(loc2) as writer:
+#	OBAdf2.to_excel(writer,sheetname,startrow=Excelrowcount+2)
+#	writer.save	
+
+outputfile = ('S:\OPERATIONS & ENGINEERING\Well DR Pressure\OBA.xlsx')
+OBAdf2.to_excel(outputfile)
+print(OBAdf2.values.tolist())
+
+wb = openpyxl.load_workbook(loc2,keep_vba=True)
+ws = wb.get_sheet_by_name(sheetname)
+wb2 = openpyxl.load_workbook(outputfile)
+ws2 = wb2.worksheets[0]
+
+for i in range(2,obadatarows):
+	for j in range(1,6):
+			ws.cell(Excelrowcount+i,j).value = (ws2.cell(i,j+1).value)
+			print(ws2.cell(i,j+1).value)
+
+wb.save(loc2)
