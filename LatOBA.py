20f4526e45f30d6d4da44a7810b9713b1b3e1f82
Zach Bateman <zkbateman@gmail.com>
2019-05-15 11:56:24 -0500

    Refactor script into functions for each section; add if __name__ == '__main__':

--- LatOBA.py
+++ LatOBA.py
@@ -19,101 +19,126 @@ with open("C:\\Users\camoruso\Desktop\password.txt", "r") as login_file:
     username = contents[0]
     password = contents[1]
 
-#Open Worksheet with Pandas
-wellsdf = pd.read_excel(loc2, sheet_name = sheetname)
-Excelrowcount = len(wellsdf)
-columns = wellsdf.columns
-Datelist = wellsdf[columns[0]].tolist()
-last = len(Datelist)
-lastdate = Datelist[last-1]
 
-today = datetime.date.today()
-Yesterday = datetime.date.today() + datetime.timedelta(days=-1)
-if lastdate.date() < Yesterday:
-	Begdate = lastdate.date() + datetime.timedelta(days=1)
-	Begdate = Begdate.strftime('%m/%d/%Y')
-	print(Begdate)
-	Enddate = Yesterday
-	Enddate = Enddate.strftime('%m/%d/%Y')
-	print(Enddate)
-else:
-	Begdate = "05/02/2019"
-	Enddate= "05/07/2019"
 
-driver = webdriver.Chrome()
-#"C:\Users\camoruso\Documents\Helpful Docs\Programming")
-driver.get('https://www.gasstorage.net/WORSHAMSTEED/Operator/index.cfm')
+def get_workbook_parameters(filename, sheet_name):
+    '''
+    Open Worksheet with Pandas
+    Return row count, Begdate, and Enddate
+    '''
+    wellsdf = pd.read_excel(filename, sheet_name=sheetname)
+    Excelrowcount = len(wellsdf)
+    columns = wellsdf.columns
+    Datelist = wellsdf[columns[0]].tolist()
+    lastdate = Datelist[-1]
 
-element = driver.find_element_by_xpath('//*[@id="username"]')
-element.send_keys(username)
+    Yesterday = datetime.date.today() + datetime.timedelta(days=-1)
+    if lastdate.date() < Yesterday:
+        Begdate = lastdate.date() + datetime.timedelta(days=1)
+        Begdate = Begdate.strftime('%m/%d/%Y')
+        print(Begdate)
+        Enddate = Yesterday
+        Enddate = Enddate.strftime('%m/%d/%Y')
+        print(Enddate)
+    else:
+        Begdate = "05/02/2019"
+        Enddate = "05/07/2019"
 
-element = driver.find_element_by_xpath('//*[@id="password"]')
-element.send_keys(password)
+    return Excelrowcount, Begdate, Enddate
 
-element = driver.find_element_by_xpath('//*[@id="frmLogin"]/table[1]/tbody/tr[3]/td/input')
-element.send_keys(Keys.RETURN)
 
-element = driver.find_element_by_xpath('//*[@id="navmenu"]/table/tbody/tr[14]/td/a')
-element.send_keys(Keys.RETURN)
+def df_from_web(Begdate, Enddate):
+    '''
+    Read data from gasstorage.net
+    Return OBAdf DataFrame.
+    '''
+    driver = webdriver.Chrome()
+    #"C:\Users\camoruso\Documents\Helpful Docs\Programming")
+    driver.get('https://www.gasstorage.net/WORSHAMSTEED/Operator/index.cfm')
 
-element = driver.find_element_by_xpath('//*[@id="NetFacilStart_Disp"]')
-element.clear()
-element.send_keys(Begdate)
+    element = driver.find_element_by_xpath('//*[@id="username"]')
+    element.send_keys(username)
 
-element = driver.find_element_by_xpath('//*[@id="NetFacilEnd_Disp"]')
-element.clear()
-element.send_keys(Enddate)
+    element = driver.find_element_by_xpath('//*[@id="password"]')
+    element.send_keys(password)
 
-element = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/fieldset[3]/form/table/tbody/tr/td[5]/input')
-element.send_keys(Keys.RETURN)
+    element = driver.find_element_by_xpath('//*[@id="frmLogin"]/table[1]/tbody/tr[3]/td/input')
+    element.send_keys(Keys.RETURN)
 
-table = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/table/tbody')
-#table2 = driver.find_elements_by_xpath('//*[@id="content"]/div/div[3]/table/tbody/tr')
-rows = table.find_elements_by_tag_name("tr")
+    element = driver.find_element_by_xpath('//*[@id="navmenu"]/table/tbody/tr[14]/td/a')
+    element.send_keys(Keys.RETURN)
 
-x = []
-for row in rows:
-	x.append([td.text for td in row.find_elements_by_tag_name("td")])
+    element = driver.find_element_by_xpath('//*[@id="NetFacilStart_Disp"]')
+    element.clear()
+    element.send_keys(Begdate)
 
-OBAdf = pd.DataFrame(x)
+    element = driver.find_element_by_xpath('//*[@id="NetFacilEnd_Disp"]')
+    element.clear()
+    element.send_keys(Enddate)
 
-#Close webdriver
-driver.close()
-driver.quit()
+    element = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/fieldset[3]/form/table/tbody/tr/td[5]/input')
+    element.send_keys(Keys.RETURN)
 
-#Count number of rows copied over
-obadatarows = len(OBAdf)
-#print(df[1:numrows2-1])
+    table = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/table/tbody')
+    #table2 = driver.find_elements_by_xpath('//*[@id="content"]/div/div[3]/table/tbody/tr')
+    rows = table.find_elements_by_tag_name("tr")
 
-#Change dataframe to chop off header and total row
-OBAdf2 = OBAdf[1:obadatarows-1]
-print(OBAdf2.dtypes)
+    x = []
+    for row in rows:
+        x.append([td.text for td in row.find_elements_by_tag_name("td")])
+    OBAdf = pd.DataFrame(x)
 
-#for col in OBAdf2.columns[2:]:
-#	OBAdf2[col] = OBAdf2[col].apply(pd.to_numeric)
-	#convert_object(convert_numeric = True)
-#print(OBAdf2.dtypes)
+    #Close webdriver
+    driver.close()
+    driver.quit()
 
-#output
-#print(df[1:numrows2])
-#writer = pd.Excelwriter(loc2)
-#Write to excel after the number of entries from origina numrows (Excel sheet count)
-#with pd.ExcelWriter(loc2) as writer:
-#	OBAdf2.to_excel(writer,sheetname,startrow=Excelrowcount+2)
-#	writer.save
+    return OBAdf
 
-outputfile = ('S:\OPERATIONS & ENGINEERING\Well DR Pressure\OBA.xlsx')
-OBAdf2.to_excel(outputfile)
-print(OBAdf2.values.tolist())
 
-wb = openpyxl.load_workbook(loc2,keep_vba=True)
-ws = wb.get_sheet_by_name(sheetname)
-wb2 = openpyxl.load_workbook(outputfile)
-ws2 = wb2.worksheets[0]
+def update_workbook(OBAdf, Excelrowcount):
+    '''
+    Update OBA.xlsx with data pulled from gasstorage.net
+    '''
+    #Count number of rows copied over
+    obadatarows = len(OBAdf)
+    #print(df[1:numrows2-1])
 
-for i in range(2,obadatarows):
-	for j in range(1,6):
-			ws.cell(Excelrowcount+i,j).value = (ws2.cell(i,j+1).value)
-			print(ws2.cell(i,j+1).value)
+    #Change dataframe to chop off header and total row
+    OBAdf2 = OBAdf[1:obadatarows-1]
+    print(OBAdf2.dtypes)
 
-wb.save(loc2)
+    #for col in OBAdf2.columns[2:]:
+    #	OBAdf2[col] = OBAdf2[col].apply(pd.to_numeric)
+        #convert_object(convert_numeric = True)
+    #print(OBAdf2.dtypes)
+
+    #output
+    #print(df[1:numrows2])
+    #writer = pd.Excelwriter(loc2)
+    #Write to excel after the number of entries from origina numrows (Excel sheet count)
+    #with pd.ExcelWriter(loc2) as writer:
+    #	OBAdf2.to_excel(writer,sheetname,startrow=Excelrowcount+2)
+    #	writer.save
+
+    outputfile = ('S:\OPERATIONS & ENGINEERING\Well DR Pressure\OBA.xlsx')
+    OBAdf2.to_excel(outputfile)
+    print(OBAdf2.values.tolist())
+
+    wb = openpyxl.load_workbook(loc2,keep_vba=True)
+    ws = wb.get_sheet_by_name(sheetname)
+    wb2 = openpyxl.load_workbook(outputfile)
+    ws2 = wb2.worksheets[0]
+
+    for i in range(2,obadatarows):
+        for j in range(1,6):
+            ws.cell(Excelrowcount+i,j).value = (ws2.cell(i,j+1).value)
+            print(ws2.cell(i,j+1).value)
+
+    wb.save(loc2)
+
+
+
+if __name__ == '__main__':
+    Excelrowcount, Begdate, Enddate = get_workbook_parameters(loc2, sheetname)
+    OBAdf = df_from_web(Begdate, Enddate)
+    update_workbook(OBAdf, Excelrowcount)
